66456637f2ef79d51996094a28b3e2c18bef6b07
xpanvictor <xpanvictor@gmail.com>
2023-04-11 23:12:08 +0100

    feat: basic flow for savings in solidity

---
+++ hardhat_contract/contracts/Bitsave.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.8.0;
+
+
+contract Bitsave {
+  // *********++++++ Storage +++++++********
+  address[] usersAddresses;
+  // *********------ Storage -------********
+
+  constructor() {
+  // All constructor functions
+  }
+
+  function createSavings(
+  ) public returns (uint) {
+    // functionality for creating savings
+  }
+
+  function withdrawSavings(
+  ) public returns (bool) {
+
+  }
+
+}
+
