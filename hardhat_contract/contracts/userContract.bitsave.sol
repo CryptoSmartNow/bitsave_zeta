0a60b762a17a81e0b54eae2cdb766020ee3b3b42
xpanvictor <xpanvictor@gmail.com>
2023-04-12 13:12:26 +0100

    feat: create savings functionality

--- hardhat_contract/contracts/userContract.bitsave.sol
+++ hardhat_contract/contracts/userContract.bitsave.sol
@@ -10,6 +10,7 @@ contract UserContract {
     // structure of saving data
     struct SavingDataStruct {
         uint256 amount;
+        uint tokenId;
         uint256 interestAccumulated;
         uint256 startTime;
         uint256 endTime;
@@ -34,8 +35,30 @@ contract UserContract {
         ownerAddress = msg.sender;
     }
 
-    function createSavings () public payable bitsaveOnly returns (string memory) {
-        return "saving A created";
+    // functionality to create savings
+    // returns: uint interest accumulated
+    function createSavings(
+        memory string name,
+        uint endTime
+    ) public payable bitsaveOnly returns (uint) {
+        uint startTime = block.timestamp;
+        // check if end time valid
+        require(endTime > startTime, "Maturity time of saving must be in the future!");
+
+        // calculate interest
+        uint accumulatedInterest = 3; // todo: create interest formulae
+
+        SavingDataStruct saving = SavingDataStruct({
+            amount : msg.value,
+            endTime : endTime,
+            interestAccumulated : accumulatedInterest,
+            startTime : startTime,
+            tokenId : 0
+        });
+
+        // store saving to map of savings
+        savings[name] = saving;
+        return saving.interestAccumulated;
     }
 
     function addToSavings () public payable bitsaveOnly returns (uint) {
