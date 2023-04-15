013e3d8a4be5748030b8c9c10aafba03214deea4
xpanvictor <xpanvictor@gmail.com>
2023-04-15 13:21:14 +0100

    feat: building up the swap feature

--- hardhat_contract/contracts/Bitsave.sol
+++ hardhat_contract/contracts/Bitsave.sol
@@ -2,26 +2,54 @@
 pragma solidity >=0.5.0 <0.8.0;
 
 
-contract Bitsave {
+contract Bitsave is ZContract {
   // *********++++++ Storage +++++++********
+
+  // ****Contract params****
+  // router02 address
+  address _router02;
+  address _usdc;
+  // ** Contract params **
+
   address[] usersAddresses;
   // mapping of userAddress to their child contract address
   mapping (address => address) addressToUserBS;
+
   // *********------ Storage -------********
 
-  constructor() {
-  // All constructor functions
+  constructor(address router02, address usdc) {
+    // All constructor functions
+    _router02 = router02;
+    _usdc = usdc;
+  }
+
+  function crossChainSwap (
+    address targetToken,
+    uint amountToSwap
+  ) internal {
+    // receive the amount to swap
+    // convert the token to targetToken
+    // return amount swapped for
   }
 
   function joinBitsave() public payable {
     // deploy child contract for user
-    address userBSAddress = 0x;
+    address userBSAddress;
     addressToUserBS[msg.sender] = userBSAddress;
   }
 
   function createSavings(
-  ) public returns (uint) {
+    // safe/risk mode
+    bool safeMode
+  ) public payable returns (uint) {
+    uint amountToSave = msg.value();
     // functionality for creating savings
+    if (!safeMode) {
+      amountToSave = crossChainSwap(
+        _usdc,
+        amountToSave
+      );
+    }
     return 3;
   }
 
