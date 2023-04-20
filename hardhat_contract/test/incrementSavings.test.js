a357c0f1c0220bce8ecbcceb9986ad315afb69d1
xpanvictor <xpanvictor@gmail.com>
2023-04-20 07:59:32 +0100

    test: contract revert's incrementing completed savings

--- hardhat_contract/test/incrementSavings.test.js
+++ hardhat_contract/test/incrementSavings.test.js
@@ -1,7 +1,8 @@
 
-const {loadFixture} = require("@nomicfoundation/hardhat-network-helpers")
+const {loadFixture, time} = require("@nomicfoundation/hardhat-network-helpers")
 const {expect} = require("chai")
 const {deployBitsaveFixture} = require("./bitsave.test")
+const {USDC_ADDRESS, ONE_GWEI} = require("../constants/config");
 
 describe("Increment savings", ()=>{
     it("Should revert if user not registered", async ()=>{
@@ -18,9 +19,45 @@ describe("Increment savings", ()=>{
         )
     })
 
-    it("Should revert if saving to increment not present")
+    it("Should revert if saving to increment not present", async()=>{
+        const {bitsave, registeredUser} = await loadFixture(deployBitsaveFixture);
 
-    it("Should revert if saving maturity time exceeded")
+        await expect(
+            bitsave
+                .connect(registeredUser)
+                .incrementSaving("savingUnused")
+        ).to.be.revertedWith("Saving to fund does not exist");
+    })
+
+    it("Should revert if saving maturity time exceeded", async()=>{
+        const {bitsave, registeredUser} = await loadFixture(deployBitsaveFixture);
+
+        const nameOfSaving = "school"
+        const useSafeMode = false;
+        const twoPenaltyPercentage = 2;
+        const amountToSave = 3 * ONE_GWEI;
+        const newTimestamp = Date.now() + 300_000;
+
+        await bitsave
+                .connect(registeredUser)
+                .createSaving(
+                    nameOfSaving,
+                    newTimestamp,
+                    twoPenaltyPercentage,
+                    useSafeMode,
+                    USDC_ADDRESS,
+                    amountToSave
+                )
+
+        // increase time with hardhat
+        await time.increaseTo(Math.round(newTimestamp / 1000));
+
+        await expect(
+            bitsave
+                .connect(registeredUser)
+                .incrementSaving(nameOfSaving)
+        ).to.be.revertedWith("Sorry, saving has ended");
+    })
 
     it("Should withdraw token of savings from user balance")
 
