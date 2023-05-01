a0438eef73677fdd5e1989b7dc9fc20eba4fa750
xpanvictor <xpanvictor@gmail.com>
2023-05-01 08:32:14 +0100

    feat: basic usd bitsave token to use in the contract as stable coin

--- hardhat_contract/contracts/udsk.sol
+++ hardhat_contract/contracts/udsk.sol
@@ -3,10 +3,10 @@
 pragma solidity >=0.7.5;
 
 // todo: get zrc contract
-import "";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract UDSK is IZRC {
-    constructor() {
-
+contract UDSK is ERC20 {
+    constructor() ERC20("USD Bitsave", "USDB") {
+        _mint(msg.sender, 20 * 10 ** 18);
     }
 }
