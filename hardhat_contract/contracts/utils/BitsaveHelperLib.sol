fb3fd1e8a6fb94c8f53f54b7fe9626400e2b2511
xpanvictor <xpanvictor@gmail.com>
2023-05-08 17:07:03 +0100

    feat: abstracts the approveAmount fn to an helper library

---
+++ hardhat_contract/contracts/utils/BitsaveHelperLib.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity = 0.8.7;
+
+library BitsaveHelperLib {
+
+  // Errors
+  error WrongGasContract();
+  error NotEnoughToPayGasFee();
+
+   function approveAmount(
+    address toApproveUserAddress,
+    uint256 amountToApprove,
+    address targetToken
+  ) internal view return(uint256) {
+    (address gasZRC20, uint256 gasFee) = IZRC20(targetToken)
+      .withdrawGasFee();
+
+      if (gasZRC20 != targetZRC20) revert WrongGasContract();
+      if (gasFee >= amount) revert NotEnoughToPayGasFee();
+
+      uint256 actualSaving = amountToApprove - gasFee;
+
+      IZRC20(targetZRC20).approve(targetZRC20, gasFee);
+      IZRC20(targetZRC20).approve(toApproveUserAddress, actualSaving);
+      return actualSaving;
+  }
+}
+
